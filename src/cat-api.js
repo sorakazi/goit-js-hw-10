e0746b24c93fe4ce1ce5c38e0fe56278da99d194
sorakazi <edmundargana@gmail.com>
2024-04-28 00:55:57 +0800

    breedSelectEl update

---
+++ src/cat-api.js
@@ -0,0 +1,23 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_mWBQFiOeKj4V3pA9w1suKA2oCAa063wtxMcHBAZYTMnzrMosvE0LuJnYSfFHIUWM';
+
+export function fetchBreeds() {
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+  ).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
-import { fetchBreeds, fetchCatByBreed } from './api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const selectEl = document.getElementById('breed-select is-hidden');
+const breedSelectEl = document.querySelector('.breed-select');
+const catInfoEl = document.querySelector('.cat-info');
+const loaderEl = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
 
 function chooseBreed() {
-  let;
+  fetchBreeds().then(data => {
+    loaderEl.classList.replace('loader', 'is-hidden');
+    // console.log(data);
+    let optionsMarkup = data.map(({ name, id }) => {
+      return `<option value=${id}>${name}</option>`;
+    });
+
+    breedSelectEl.insertAdjacentHTML('beforeend', optionsMarkup);
+    breedSelectEl.classList.remove('is-hidden');
+  });
 }
+chooseBreed();
+
+breedSelectEl.addEventListener('change', e => {
+  // alert(e.target.value);
+  let breedId = e.target.value;
+  fetchCatByBreed(breedId).then(data => {
+    const { url, breeds } = data[0];
+    const { name, description, temperament } = breeds[0];
+    catInfoEl.innerHTML = `<img src ='${url}' alt ='{name}'  width ="400"/>
+    <div class= 'box'>
+    <h2>${name}</h2>
+    <h2>${description}</h2>
+    <h2>${temperament}</h2>
+    </div>
+    `;
+  });
+});
+// function chooseBreed(data) {
+//   fetchBreeds(data).then(data => {
+//     //   console.log(data);
+
+//     let optionsMarkup = data.map(({ name, id }) => {
+//       return `<option value ='${id}'>${name}</option>`;
+//     });
+
+//     breedSelectEl.insertAdjacentHTML('beforeend', optionsMarkup);
+//     breedSelectEl.classList.remove('is-hidden'); // Show select element after options are added
+//   });
+// }
+// chooseBreed();
